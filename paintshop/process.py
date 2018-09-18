d382227ca70bbe5404b3a997c5acc881d48b6d31
AlEmerich <alan.guitard.pro@gmail.com>
2018-09-18 23:52:45 +0200

    done

--- paintshop/process.py
+++ paintshop/process.py
@@ -1,16 +1,52 @@
+"""Package to answer to retrive set of colors to please every customers.
+"""
 import numpy as np
+
+def is_satisfied(colorset, pref):
+    """Return a tuple (boolean, num_color) if the customer's
+    preference match with the specified colorset. If False, num_color
+    is the color to change in the colorset."""
+    num_m = -1
+
+    for i in range(len(pref) // 2):
+        num = int(pref[i*2]) - 1
+        color = pref[i*2+1]
+        if colorset[num] == color:
+            return True, -1
+        if color is 'M':
+            num_m = num
+    return False, num_m
 
 
 def get_optimal_set(path):
-    with open(path, 'w') as f:
-        contents = int(f.readlines().strip())
+    """Get the colorset who please every customer,
+    or No solution exists if there is none.
+    """
+    with open(path, 'r') as f:
+        contents = f.readlines()
 
+    # Get the number of color
     num_of_color = int(contents[0].strip())
 
-    result = np.empty(num_of_color)
-    result.fill('G')
+    # Initialize with gloss color, because we
+    # want to minimize matte color
+    result = ['G' for x in range(num_of_color)]
 
-    for lines in contents[1:]:
-        l_arr = lines.strip().split(" ")
+    # Two steps:
+    # 1: The first pass is used to change the colorset, to please each consumer.
+    # 2: The second pass is used to validate the result colorset. If someone is not
+    #    pleased anymore, that means it is conflicted with another customers so there
+    #    is no solution.
+    # Note: The lines in the files must be sorted by length to make this algorithm
+    #       works.
+    for step in range(2):
+        for lines in sorted(contents[1:], key=len):
+            l_arr = lines.strip().split(" ")
+            satisfied, num_c = is_satisfied(result, l_arr)
+            if not satisfied:
+                if step == 0:
+                    result[num_c] = 'M'
+                else:
+                    return "No solution exists"
 
     return result
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,16 +3,20 @@ from paintshop.process import get_optimal_set
 
 def test_no_solution():
     colorset = get_optimal_set("no_solution.txt")
-    assert colorset == "No solution exists", "A solution is found where there is none"
+    assert colorset == "No solution exists", \
+        "A solution is found where there is none: " + str(colorset)
 
 def test_first_example():
     colorset = get_optimal_set("first.txt")
-    assert colorset == ['G', 'G', 'G', 'G', 'M'], "Wrong solution"
+    assert colorset == ['G', 'G', 'G', 'G', 'M'], \
+        "Wrong solution: " + str(colorset)
 
 def test_second_example():
     colorset = get_optimal_set("second.txt")
-    assert colorset == ['G','M','G','M','G'], "Wrong solution"
+    assert colorset == ['G','M','G','M','G'], \
+        "Wrong solution: " + str(colorset)
 
 def test_third_example():
-    colorset = get_optimal_set("second.txt")
-    assert colorset == ['M', 'M'], "Wrong solution"
+    colorset = get_optimal_set("third.txt")
+    assert colorset == ['M', 'M'], \
+        "Wrong solution: " + str(colorset)
